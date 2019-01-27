90c01efaa43817373ae6d3907b734baab230d224
Nick Stames <nstames0@gmail.com>
2019-01-27 18:27:41 -0500

    Fixed some stuff
    rreal descriptive actually I forgot what I fixed but I think it actually broke stuff,

--- ImagePlugin/src/me/electro/CommandLoadImage.java
+++ ImagePlugin/src/me/electro/CommandLoadImage.java
@@ -12,6 +12,7 @@ import javax.imageio.ImageIO;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -27,8 +28,7 @@ import org.imgscalr.Scalr; // extremely simple imaging library. does antialiased
 public class CommandLoadImage implements CommandExecutor {
 
 	@Override
-	public boolean onCommand(CommandSender commandSender, Command command, String c, String[] args) { //FIXME flat chunk issue, add replace option
-		//if(commandSender.hasPermission("imageplugin.loadimage")) //FIXME
+	public boolean onCommand(CommandSender commandSender, Command command, String c, String[] args) { //FIXME flat chunk issue, fix lighting maybe maybe, maybe do this asynchonously?
 		// first, find the player and make sure it's not sent from console
 		Player sender = null;
 		for(Player p : Bukkit.getOnlinePlayers()) {
@@ -39,12 +39,9 @@ public class CommandLoadImage implements CommandExecutor {
 		if(sender == null) {
 			commandSender.sendMessage(ChatColor.RED + "Bro, need to send from a player.");
 			return false;
+			
 		}
-		if(args.length == 0)  {//XXX 
-			commandSender.sendMessage(ChatColor.LIGHT_PURPLE + String.format("%.1f", sender.getLocation().getYaw()));
-			return true;
-		}
-		
+
 		
 		// second, make sure the command is syntactically and semantically valid
 		if(args.length < 4) // need at least a url, an orientation, a direction, and dithering
@@ -52,29 +49,30 @@ public class CommandLoadImage implements CommandExecutor {
 		String url = args[0];
 		String orientation = args[1];
 		String direction = args[2];
-		String dithering = args[3];
 		boolean vertical = false;
+		boolean dither = false;
+		boolean replace = false;
+		boolean barriers = false;
 		if(orientation.equalsIgnoreCase("stand")) {
 			if(!direction.equalsIgnoreCase("left") && !direction.equalsIgnoreCase("right")) {
-				commandSender.sendMessage(ChatColor.RED + "Orientation '" + orientation + "' must be one of 'left', 'right' since orientation is 'stand'.");
+				commandSender.sendMessage(ChatColor.RED + "Orientation '" + orientation + "' must be one of 'left', 'right' (since orientation is 'stand'.)");
 				return false;
 			}
 			vertical = true;
 		} else if(orientation.equalsIgnoreCase("flat")) {
 			if(!direction.equalsIgnoreCase("bottom") && !direction.equalsIgnoreCase("top")) {
-				commandSender.sendMessage(ChatColor.RED + "Diretion '" + orientation + "' must be one of 'bottom', 'top' since orientation is 'flat'.");
+				commandSender.sendMessage(ChatColor.RED + "Diretion '" + orientation + "' must be one of 'bottom', 'top' (since orientation is 'flat'.)");
 				return false;
 			} //vertical = false
 		} else {
 			commandSender.sendMessage(ChatColor.RED + "Direction '" + orientation + "' must be one of 'stand', 'flat'.");
 			return false;
 		}
-		boolean dither = Boolean.parseBoolean(dithering); // returns false for anything but "true", ignoring case
 		
 		
 		int widthPreferred = -1, heightPreferred = -1; // all of these are optional parameters, width and height will be maxed by default, rotation default is 0
 		Scalr.Rotation rotPreferred = null;
-		for(int i = 4; i < args.length; i++) { // get the rest of the args. max possibility of 3 other args
+		for(int i = 3; i < args.length; i++) { // get the rest of the args. max possibility of 3 other args
 			String keywordPair = args[i];
 			if(keywordPair.contains("=")) { // valid keyword pair
 				String[] keywordPairArray = keywordPair.split("=");
@@ -125,8 +123,14 @@ public class CommandLoadImage implements CommandExecutor {
 					commandSender.sendMessage(ChatColor.RED + "Unexpected '=': " + keywordPair + ".");
 					return false;
 				}
+			} else if(keywordPair.equalsIgnoreCase("dither")) {
+				dither = true;
+			} else if(keywordPair.equalsIgnoreCase("replace")) {
+				replace = true;
+			} else if(keywordPair.equalsIgnoreCase("barriers")) {
+				barriers = true;
 			} else {
-				commandSender.sendMessage(ChatColor.RED + "Parameter '" + keywordPair + "' has no key assigned. Should be one of 'w=', 'h=', 'r='.");
+				commandSender.sendMessage(ChatColor.RED + "Unknown option '" + keywordPair + "'. Should be one of 'dither', 'replace', 'barriers'. Parameters require an '=' otherwise.");
 				return false;
 			}
 		}
@@ -171,6 +175,10 @@ public class CommandLoadImage implements CommandExecutor {
 			widthPreferred = imgWidth; // now these 100% refer to the width/height that will be in-game
 			heightPreferred = imgHeight;
 			commandSender.sendMessage(ChatColor.GRAY + String.format("Retrieved image successfully. Size is %dx%d", widthPreferred, heightPreferred));
+		}
+		if(widthPreferred > 400 || heightPreferred > 400) {
+			commandSender.sendMessage(ChatColor.RED + "Image size too large. Resize to smaller than 400x400. You wouldn't be able to see the whole thing anyway.");
+			return true; // not a usage error
 		}
 		
 		
@@ -235,7 +243,6 @@ public class CommandLoadImage implements CommandExecutor {
 		
 		// test if the image is not high enough or too high up
 		startingLoc = heightDir.move(startingLoc, 1 - heightPreferred); // move to the top if it is vertical or move out to the height if horizontal
-		commandSender.sendMessage(ChatColor.LIGHT_PURPLE + "" + startingLoc.getBlock());
 		if(startingLoc.getBlockY() > 255) {
 			int maxHeight = heightPreferred - startingLoc.getBlockY() + 255; // calculate maximum height to reach world limit
 			int maxWidth = (int) ((float) imgWidth / imgHeight * maxHeight + 0.5f); // calculate ratio and compute new width. round to the nearest int
@@ -250,24 +257,25 @@ public class CommandLoadImage implements CommandExecutor {
 		// see if there is enough room in the world (i.e. the image won't destroy any blocks)
 		// now that we're at the top of the image (startingLoc), we can just loop through all the image block locations
 		Location loc = startingLoc.clone();
-		int badBlocks = 0;
-		for(int dH = 0; dH < heightPreferred; dH++) {
-			for(int dW = 0; dW < widthPreferred; dW++) { // after each width loop
-				Block b = loc.getBlock();
-				if(!blockOk(b)) { // avoid solid blocks. blocks such as snow and grass should be okay to destroy
-					badBlocks++;
+		if(!replace) { // if we are replacing then we don't care if there's blocks in the way
+			int badBlocks = 0;
+			for(int dH = 0; dH < heightPreferred; dH++) {
+				for(int dW = 0; dW < widthPreferred; dW++) { // after each width loop
+					Block b = loc.getBlock();
+					if(!blockOk(b)) { // avoid solid blocks. blocks such as snow and grass should be okay to destroy
+						badBlocks++;
+					}
+					
+					loc = widthDir.move(loc, 1); // move 1 time in the width direction * widthPreferred times
 				}
-				
-				loc = widthDir.move(loc, 1); // move 1 time in the width direction * widthPreferred times
+				loc = widthDir.move(loc, -widthPreferred); // move widthPreferred times in the negative direction * 1 time
+				loc = heightDir.move(loc, 1); // move 1 time in the height direction * heightPreferred times
 			}
-			loc = widthDir.move(loc, -widthPreferred); // move widthPreferred times in the negative direction * 1 time
-			loc = heightDir.move(loc, 1); // move 1 time in the height direction * heightPreferred times
+			if(badBlocks > 0) {
+				commandSender.sendMessage(ChatColor.RED + "There are " + badBlocks + " blocks in the construction path of the image. Please remove them or find another location.");
+				return true; // not a usage error
+			}
 		}
-		if(badBlocks > 0) {
-			commandSender.sendMessage(ChatColor.RED + "There are " + badBlocks + " blocks in the construction path of the image. Please remove them or find another location.");
-			return true; // not a usage error
-		}
-		
 		
 		if(needsResizing) {
 			image = Scalr.resize(image, Scalr.Method.QUALITY, widthPreferred, heightPreferred, Scalr.OP_ANTIALIAS); // resize to the preferred and/or calculated height
@@ -277,23 +285,12 @@ public class CommandLoadImage implements CommandExecutor {
 		}
 		
 		
-		// place barrier blocks underneath the image where needed to prevent falling sand ... well ... falling
-		if(vertical) { // place blocks only on the bottom ridge
-			loc = playerLoc.clone();
-			loc = widthDir.move(loc, 1); // move one accross to be at the width level the image actually starts
-			loc = loc.add(0, -1, 0); // move one down in the Y to place the barriers below the image
-			for(int dW = 0; dW < widthPreferred; dW++) { // after each width loop
-				Block block = loc.getBlock();
-				if(blockOk(block)) {
-					block.setType(Material.BARRIER);
-				}
-				
-				loc = widthDir.move(loc, 1); // move 1 time in the width direction * widthPreferred times
-			}
-		} else { // place blocks all under the entire image
-			loc = startingLoc.clone();
-			loc = loc.add(0, -1, 0); // move one down in the Y to place the barriers below the image
-			for(int dH = 0; dH < heightPreferred; dH++) {
+		// place barrier blocks underneath the image where needed to prevent falling sand ... well ... falling (this is only needed if they will update the blocks)
+		if(barriers) {
+			if(vertical) { // place blocks only on the bottom ridge
+				loc = playerLoc.clone();
+				loc = widthDir.move(loc, 1); // move one accross to be at the width level the image actually starts
+				loc = loc.add(0, -1, 0); // move one down in the Y to place the barriers below the image
 				for(int dW = 0; dW < widthPreferred; dW++) { // after each width loop
 					Block block = loc.getBlock();
 					if(blockOk(block)) {
@@ -302,20 +299,40 @@ public class CommandLoadImage implements CommandExecutor {
 					
 					loc = widthDir.move(loc, 1); // move 1 time in the width direction * widthPreferred times
 				}
-				loc = widthDir.move(loc, -widthPreferred); // move widthPreferred times in the negative direction * 1 time
-				loc = heightDir.move(loc, 1); // move 1 time in the height direction * heightPreferred times
+			} else { // place blocks all under the entire image
+				loc = startingLoc.clone();
+				loc = loc.add(0, -1, 0); // move one down in the Y to place the barriers below the image
+				for(int dH = 0; dH < heightPreferred; dH++) {
+					for(int dW = 0; dW < widthPreferred; dW++) { // after each width loop
+						Block block = loc.getBlock();
+						if(blockOk(block)) {
+							block.setType(Material.BARRIER);
+						}
+						
+						loc = widthDir.move(loc, 1); // move 1 time in the width direction * widthPreferred times
+					}
+					loc = widthDir.move(loc, -widthPreferred); // move widthPreferred times in the negative direction * 1 time
+					loc = heightDir.move(loc, 1); // move 1 time in the height direction * heightPreferred times
+				}
 			}
 		}
 		
 		
 		// image is ready to be dithered using a floyd-steinburg implementation and placed into the world at the same time while we gather the correct materials to use
 		loc = startingLoc.clone();
+		int chunksNeededLoading = 0;
 		for(int dH = 0; dH < heightPreferred; dH++) {
 			for(int dW = 0; dW < widthPreferred; dW++) { // after each width loop
 				int rgb = image.getRGB(dW, dH);
 				Color originalPixel = new Color(rgb);
 				Color quantizedPixel = originalPixel.quantize();
 				Material bestBlockMatch = Main.colorMap.get(quantizedPixel);
+				Block block = loc.getBlock();
+				Chunk chunk = block.getChunk();
+				if(!chunk.isLoaded()) {
+					chunk.load(true); // load the chunk if needed, generating as necessary
+					chunksNeededLoading++;
+				}
 				loc.getBlock().setType(bestBlockMatch, false); // set the material in the world, no gravity for falling blocks
 				if(bestBlockMatch == Material.BROWN_MUSHROOM_BLOCK) {
 					// need to set some metadata for the mushroom
@@ -338,8 +355,11 @@ public class CommandLoadImage implements CommandExecutor {
 			loc = widthDir.move(loc, -widthPreferred); // move widthPreferred times in the negative direction * 1 time
 			loc = heightDir.move(loc, 1); // move 1 time in the height direction * heightPreferred times
 		}
-		
-		//TODO: and do all this from a thread probably otherwise it might lag the server?
+		if(chunksNeededLoading > 0) {
+			commandSender.sendMessage(ChatColor.GREEN + "Generated image successfully. Loaded " + chunksNeededLoading + " in the process.");
+		} else {
+			commandSender.sendMessage(ChatColor.GREEN + "Generated image successfully.");
+		}
 		return true; // success!
 	}
 	
